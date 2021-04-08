ff14be5a3d2a58e59636667f36fbe910c8964c2c
Student <Student@DESKTOP-3PSC3DP>
2021-04-08 14:09:44 -0500

    Combat update

---
+++ DungeonLibrary/Combat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Combat
+    {
+        public static void DoAttack(Characters attacker, Characters defender)
+        {
+            Random rand = new Random();
+            int diceRoll = rand.Next(1, 101);
+            System.Threading.Thread.Sleep(1500);
+
+            if (diceRoll <= (attacker.CalcHitChance() - defender.CalcBlock()))
+            {
+                int damageDelt = attacker.CalcDamage();
+
+                defender.Life -= damageDelt;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} hit {1} for {2} damage!", attacker.Name, defender.Name, damageDelt);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine($"{attacker.Name} missed!");
+            }
+        }//enddoattack
+
+        public static void DoBattle(Player player, Opponents opponents)
+        {
+            DoAttack(player, opponents);
+
+            if (opponents.Life > 0)
+            {
+                DoAttack(opponents, player);
+            }
+        }
+
+    }
+}
---
+++ DungeonLibrary/Mongol.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Mongol : Opponents
+    {
+        public Mongol(string name, int life, int maxLife, int hitChance, int block, int minDamage, int maxDamage, string description) : base(name, life, maxLife, hitChance, block, minDamage, maxDamage, description)
+        {
+           
+        }
+
+        public Mongol()
+        {
+            MaxLife = 40;
+            MaxDamage = 12;
+            Name = "Mongol";
+            Life = 30;
+            HitChance = 20;
+            Block = 15;
+            MinDamage = 4;
+            Description = "This should be easy";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override int CalcBlock()
+        {
+            int calculatedBlock = Block;
+
+            return calculatedBlock;
+        }
+
+    }
+}
